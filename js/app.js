e86e40006804674959f9afa9d589b1eb97495d46
JGuerrero-at-014991514483 <jguerrero@ercoenergia.com.co>
2021-11-04 09:31:54 -0500

    project start

---
+++ js/app.js
@@ -0,0 +1,27 @@
+var app = new Framework7({
+    // App root element
+    el: '#app',
+    // App Name
+    name: 'My App',
+    // App id
+    id: 'com.myapp.test',
+    // Enable swipe panel
+    panel: {
+      swipe: true,
+    },
+    // Add default routes
+    routes: [
+      {
+        path: '/about/',
+        url: 'about.html',
+      },
+      {
+        path:'/ps-s1/',
+        url: 'ps-s1.html/', 
+
+      },
+    ],
+    // ... other parameters
+  });
+  
+  var mainView = app.views.create('.view-main')
